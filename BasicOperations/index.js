e33748d43532d1443ca8303267172b36f85d7a27
Richard Solymosi <solymosi.richard95@gmail.com>
2023-11-29 19:26:02 +0100

    fraction multiplication.

--- BasicOperations/index.js
+++ BasicOperations/index.js
@@ -10,6 +10,8 @@ const operationsDiv5 = document.querySelector("#operations5");
 const operationsDiv6 = document.querySelector("#operations6");
 const operationsDiv7 = document.querySelector("#operations7");
 const operationsDiv8 = document.querySelector("#operations8");
+const generateButton5 = document.querySelector("#generateButton5");
+const operationsDiv9 = document.querySelector("#operations9");
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -22,18 +24,18 @@ generateButton.addEventListener("click", e => {
     let number2 = "";
     s+=`<div class="container">`;
     for (let i = 0; i < 5; i++) {
-        number1 = getRandomNumber(100,9999);
-        number2 = getRandomNumber(100,9999);
+        number1 = getRandomNumber(100,999);
+        number2 = getRandomNumber(100,999);
         s+=`<div><span>${number1} + ${number2} =<span></div>`;
     }
     s+="</div>";
 
     s+=`<div class="container">`;
     for (let i = 0; i < 5; i++) {
-        number1 = getRandomNumber(100,9999);
-        number2 = getRandomNumber(100,9999);
+        number1 = getRandomNumber(100,999);
+        number2 = getRandomNumber(100,999);
         while (number1 === number2) {
-            number2 = getRandomNumber(100,9999);
+            number2 = getRandomNumber(100,999);
         }
         if (number2 > number1) {
             let tempNum = number1
@@ -440,3 +442,90 @@ generateButton4.addEventListener("click", e => {
 
 })
 
+generateButton5.addEventListener("click", e => {
+    operationsDiv9.innerHTML = "";
+    let s = "";
+    let number1 = "";
+    let number2 = "";
+    let number3 = "";
+
+    s+=`<div class="division3">`;
+
+    s+=`<div class="subdivision3">`;
+    for (let i = 0; i < 10; i++) {
+        number1 = getRandomNumber(2,12);
+        number2 = getRandomNumber(1,20);
+        number3 = getRandomNumber(1,40);
+
+        s+=`<div>`;
+
+        
+        s+=`<p>
+            <span>${number1}</span> 
+            * 
+            <span class="frac"><sup>${number2}</sup><span></span><sub>${number3}</sub></span>
+            = 
+            <span class="frac longfrac"><sup></sup><span></span><sub></sub></span>
+            = 
+            <span class="frac shortfrac"><sup></sup><span></span><sub></sub></span>
+            </p>`;
+
+        s+=`</div>`;
+        
+        
+    }
+    s+=`</div>`;
+    
+    s+=`<div class="subdivision3">`;
+    for (let i = 0; i < 10; i++) {
+        number1 = getRandomNumber(1,20);
+        number2 = getRandomNumber(1,40);
+        number3 = getRandomNumber(2,12);
+
+        s+=`<div>`;
+
+        
+        s+=`<p>
+            <span class="frac"><sup>${number1}</sup><span></span><sub>${number2}</sub></span> 
+            * 
+            <span>${number3}</span>
+            = 
+            <span class="frac longfrac"><sup></sup><span></span><sub></sub></span>
+            = 
+            <span class="frac shortfrac"><sup></sup><span></span><sub></sub></span>
+            </p>`;
+
+        s+=`</div>`;
+        
+    }
+    s+=`</div>`;
+
+    s+=`<div class="subdivision3">`;
+    for (let i = 0; i < 10; i++) {
+        number1 = getRandomNumber(1,10);
+        number2 = getRandomNumber(1,10);
+        number3 = getRandomNumber(1,10);
+        number4 = getRandomNumber(1,10);
+
+        s+=`<div>`;
+
+        
+        s+=`<p>
+            <span class="frac"><sup>${number1}</sup><span></span><sub>${number2}</sub></span> 
+            * 
+            <span class="frac"><sup>${number3}</sup><span></span><sub>${number4}</sub></span>
+            = 
+            <span class="frac longfrac"><sup></sup><span></span><sub></sub></span>
+            = 
+            <span class="frac shortfrac"><sup></sup><span></span><sub></sub></span>
+            </p>`;
+
+        s+=`</div>`;
+        
+    }
+    s+=`</div>`;
+
+    s+=`</div>`;
+
+    operationsDiv9.innerHTML = s;
+})
